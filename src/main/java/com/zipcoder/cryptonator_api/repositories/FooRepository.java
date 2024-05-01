fe5880bdc21df8ad9af6c0de671e227996cfbe5c
Matt Kramer <matthew123003@gmail.com>
2024-05-01 06:31:42 -0400

    Changed Repo to interface

--- src/main/java/com/zipcoder/cryptonator_api/repositories/FooRepository.java
+++ src/main/java/com/zipcoder/cryptonator_api/repositories/FooRepository.java
@@ -9,59 +9,6 @@ import java.util.Currency;
  * Created by leon on 1/22/18.
  */
 @Repository
-public class FooRepository implements CrudRepository<Currency, Long> {
-    @Override
-    public <S extends Currency> S save(S s) {
-        return null;
-    }
-
-    @Override
-    public <S extends Currency> Iterable<S> save(Iterable<S> iterable) {
-        return null;
-    }
-
-    @Override
-    public Currency findOne(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public boolean exists(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public Iterable<Currency> findAll() {
-        return null;
-    }
-
-    @Override
-    public Iterable<Currency> findAll(Iterable<Long> iterable) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void delete(Long aLong) {
-
-    }
-
-    @Override
-    public void delete(Currency currency) {
-
-    }
-
-    @Override
-    public void delete(Iterable<? extends Currency> iterable) {
-
-    }
-
-    @Override
-    public void deleteAll() {
-
-    }
+public interface FooRepository extends CrudRepository<Currency, Long> {
+    
 }
